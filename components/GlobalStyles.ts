672f3588ec05dc75419a956aa5d57709d51ae574
Ana <anafpmeira@gmail.com>
2023-12-17 19:38:47 +0000

    adding global styling

---
+++ components/GlobalStyles.ts
@@ -0,0 +1,28 @@
+/* fonts */
+export const FontFamily = {
+  bebasNeueRegular: "BebasNeue-Regular",
+  interRegular: "Inter-Regular",
+  interSemiBold: "Inter-SemiBold",
+  interMedium: "Inter-Medium",
+  robotoMedium: "Roboto-Medium",
+};
+
+  /* font sizes */
+export const FontSize = {
+  size_mini: 15,
+  size_base: 16,
+};
+
+/* Colors */
+export const Color = {
+  m3RefNeutralNeutral10: "#1d1b20",
+  colorGray_100: "#18181a",
+  colorGray_200: "rgba(255, 255, 255, 0.55)",
+  colorBlack: "#000",
+};
+
+/* border radiuses */
+export const Border = {
+  br_xs: 12,
+  br_6xl: 25,
+};
