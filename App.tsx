29f21e1dcc72482cabf5e214d483685cfd12b362
Ana <anafpmeira@gmail.com>
2023-12-28 18:22:10 +0000

    new app

--- App.tsx
+++ App.tsx
@@ -6,16 +6,40 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_AUTH } from './src/firebase/firebaseConfig';
 
-import MyStack from './components/MyStack';
+import { Login, LoginScreen, HomeScreen, ProfileScreen, RegistrationScreen } from './src/screens/index';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
+  const [user, setUser] = useState<User | null>(null); // Update the type of user state
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log('user', user);
+      setUser(user); // Update the user state
+    });
+  }, []);
+
   return (
-    //https://reactnative.dev/docs/navigation
     <NavigationContainer>
-      <MyStack />
+      {user ? (
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="ProfileScreen" component={ProfileScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
